abe9c66818a5d470abfad86783f97e11d7d2320b
pawurb23 <pawel.urbaniak23@gmail.com>
2026-01-07 01:13:20 +0100

    zła zmiana

--- MójProjekt/ProstyUAR.cpp
+++ MójProjekt/ProstyUAR.cpp
@@ -1,16 +1,37 @@
 #include "ProstyUAR.h"
 
-ProstyUAR::ProstyUAR() : model({-1.0}, {1.0}, 1, 0.0), regulator(1.0, 0.0, 0.0) {
+ProstyUAR::ProstyUAR() : model({ -1.0 }, { 1.0 }, 1, 0.0), regulator(1.0, 0.0, 0.0) {
 
-	y_ost = 0.0;
+    reset();
 }
 
 double ProstyUAR::symuluj(double y_zad) {
 
-	double uchyb = y_zad - y_ost;
-	double u_wej = regulator.symuluj(uchyb);
-	double y_wyj = model.symuluj(u_wej);
+    double uchyb = y_zad - y_ost;
+    double u_wej = regulator.symuluj(uchyb);
+    double y_wyj = model.symuluj(u_wej);
 
-	y_ost = y_wyj;
-	return y_wyj;
+    y_ost = y_wyj;
+    u_akt = u_wej;
+    e_akt = uchyb;
+
+    return y_wyj;
+}
+
+void ProstyUAR::reset() {
+    y_ost = 0.0;
+    u_akt = 0.0;
+    e_akt = 0.0;
+    regulator.resetPamieci();
+}
+
+void ProstyUAR::setPID(double k, double ti, double td) {
+    regulator.setNastawy(k, ti, td);
+}
+
+void ProstyUAR::setModel(std::vector<double> A, std::vector<double> B, int k, double z) {
+    model.setWspA(A);
+    model.setWspB(B);
+    model.setOpoznienie(k);
+    model.setZaklocenia(z);
 }
--- MójProjekt/ProstyUAR.h
+++ MójProjekt/ProstyUAR.h
@@ -9,13 +9,22 @@ private:
 	ModelARX model;
 	RegulatorPID regulator;
 	double y_ost;
+	double u_akt;
+	double e_akt;
 
 public:
 
 	ProstyUAR();
 
 	double symuluj(double y_zad);
+	void reset();
 
 	ModelARX& getModel() { return model; }
 	RegulatorPID& getRegulator() { return regulator; }
+	double getU() const { return u_akt; }
+	double getE() const { return e_akt; }
+
+	void setPID(double k, double ti, double td);
+	void setModel(std::vector<double> A, std::vector<double> B, int k, double z);
+
 };
